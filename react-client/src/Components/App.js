c44e1e6b94b653922a2837684765ac75d56146aa
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2021-09-01 15:28:14 +0800

    Task 11 retain dom info

--- react-client/src/Components/App.js
+++ react-client/src/Components/App.js
@@ -5,16 +5,16 @@ import DomInfoDialogBox from "./DomInfoDialogBox";
 import { PRODUCTION_MODE } from "../keys";
 
 function App() {
-  // const [coordinates, setCoordinates] = useState({ top: 0, left: 0 });
+  const [notes, setNotes] = useState([]);
   const [domInfo, setDomInfo] = useState({
-    id: "",
+    id: [],
     class: "",
     childcount: "",
     sample: "",
     parentID: "",
     parentClass: "",
     child: { ids: [], classes: [], totalCount: 0 },
-    coordinates: { top: 10, left: 10 },
+    coordinates: { top: [], left: [] },
   });
 
   useEffect(() => {
@@ -24,8 +24,9 @@ function App() {
        * https://web.archive.org/web/20131014212210/http://stackoverflow.com/
        */
       // getPageContent('https://www.redfin.com/');
-      getPageContent('https://web.archive.org/web/20131014212210/http://stackoverflow.com/'); 
-      
+      getPageContent(
+        "https://web.archive.org/web/20131014212210/http://stackoverflow.com/"
+      );
     } else {
       injectDOMEventInBody();
     }
@@ -33,59 +34,111 @@ function App() {
     return () => {};
   }, []);
 
-
   const injectDOMEventInBody = async () => {
-
     document.addEventListener("click", (e) => {
-      if (e.target.id !== "divDevTools") {
+      if (e.target.id !== "closedompeeker") {
         e.preventDefault();
 
-        const reduceChild = [...e.target.children].reduce((init, curr) => {
-          init.ids.push(curr.id);
-          init.classes.push(curr.className);
-          return init;
-        },{ids:[],classes:[]});
+        const reduceChild = [...e.target.children].reduce(
+          (init, curr) => {
+            init.ids.push(curr.id);
+            init.classes.push(curr.className);
+            return init;
+          },
+          { ids: [], classes: [] }
+        );
 
-        setDomInfo({
-          ...domInfo,
-          id: e.target.id,
-          class: e.target.className,
-          child: {...reduceChild, totalCount: e.target.childElementCount},
-          parentID: e.target.parentElement.id,
-          parentClass: e.target.parentElement.className,
-          coordinates: { top: e.pageY, left: e.pageX },
-        });
+        //My work around state  requesting discussion regarding state
+        setNotes((notes) => [
+          ...notes,
+          {
+            x: e.pageX,
+            y: e.pageY,
+            id: e.target.id,
+            clsname: e.target.className,
+            child: { ...reduceChild, totalCount: e.target.childElementCount },
+            parentID: e.target.parentElement.id,
+            parentClass: e.target.parentElement.className,
+          },
+        ]);
+        //My work around state  requesting discussion regarding state
+
+        // setDomInfo({
+        //   ...domInfo,
+        //   id: e.target.id,
+        //   clsname: e.target.className,
+        //   child: { ...reduceChild, totalCount: e.target.childElementCount },
+        //   parentID: e.target.parentElement.id,
+        //   parentClass: e.target.parentElement.className,
+        //   coordinates: { top: e.pageY, left: e.pageX },
+        // });
       }
     });
-  }
+  };
 
   const getPageContent = async (url) => {
-    
-    if (!localStorage.getItem('webpage')) {
-      const axios = await import('axios');
+    if (!localStorage.getItem("webpage")) {
+      const axios = await import("axios");
 
       const res = await axios.get(url);
-      localStorage.setItem('webpage', res.data);      
+      localStorage.setItem("webpage", res.data);
     }
 
-    document.getElementById('samplePage').innerHTML = localStorage.getItem('webpage');
+    document.getElementById("samplePage").innerHTML =
+      localStorage.getItem("webpage");
     injectDOMEventInBody();
-  }
+  };
 
   return (
     <div>
       {/* website page renders here... */}
       {!PRODUCTION_MODE && <div id="samplePage"></div>}
+      <div>
+        {notes.map((note) => {
+          const handleDelete = () => {
+            setNotes((prevNotes) =>
+              prevNotes.reduce(
+                (init, curr) =>
+                  curr.x === note.x && curr.y === note.y
+                    ? init
+                    : init.push(curr) && init,
+                []
+              )
+            );
+          };
 
-      <DomInfoDialogBox
-        id={domInfo.id}
-        clsname={domInfo.clsname}
-        parentId={domInfo.parentID}
-        parentClass={domInfo.parentClass}
-        count={domInfo.count}
-        child={domInfo.child}
-        coordinates={domInfo.coordinates}       
-      />
+          return (
+            <div>
+              {/* <DomInfoDialogBox
+                id={domInfo.id}
+                clsname={domInfo.clsname}
+                parentId={domInfo.parentID}
+                parentClass={domInfo.parentClass}
+                count={domInfo.count}
+                child={domInfo.child}
+                coordinates={domInfo.coordinates}
+                top={note.y}
+                left={note.x}
+                closedialog={handleDelete}
+                noteid={note.myid}
+                
+              /> */}
+              <DomInfoDialogBox
+                id={note.id}
+                clsname={note.clsname}
+                parentId={note.parentID}
+                parentClass={note.parentClass}
+                // count={domInfo.count}
+                child={note.child}
+                coordinates={domInfo.coordinates} //used in old code
+                top={note.y}
+                left={note.x}
+                closedialog={handleDelete}
+              />
+            </div>
+          );
+        })}
+      </div>
 
       {/* 
       // TODO by Sonny: Build DevTools to change webpage inside localhost dynamically
--- react-client/src/Components/DomInfoDialogBox.js
+++ react-client/src/Components/DomInfoDialogBox.js
@@ -5,9 +5,12 @@ const DomInfoDialogBox = ({
   clsname,
   parentId,
   parentClass,
-  count,
+  // count,
   child,
   coordinates,
+  top,
+  left,
+  closedialog,
 }) => {
   return (
     <div>
@@ -22,17 +25,26 @@ const DomInfoDialogBox = ({
           border: "3px solid green",
           borderRadius: "20px",
           position: "absolute",
-          top: `${coordinates.top}px`,
-          left: `${coordinates.left}px`,
+          // top: `${coordinates.top}px`,
+          // left: `${coordinates.left}px`,
+          top: `${top}px`,
+          left: `${left}px`,
         }}
       >
+        <div style="float:right">
+          <button id="closedompeeker" onClick={closedialog}>
+            X
+          </button>
+        </div>
+
         <h1>Element ID: {id}</h1>
         <h2>Class: {clsname}</h2>
         <h2>Parent ID: {parentId}</h2>
         <h2>Parent Class: {parentClass}</h2>
         <h2># of Children Element: {child.totalCount}</h2>
+
         <p>Child element ids:</p>
-        
+
         <ul>
           {child.ids.map((val) => (
             <li>{val}</li>
