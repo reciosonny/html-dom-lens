effeebc96d587c1b236acdb85a1731ac6bf3076a
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2021-09-13 16:27:43 +0800

    Task-2-Updates

--- react-client/src/Components/DomInfoDialogBox.js
+++ react-client/src/Components/DomInfoDialogBox.js
@@ -1,13 +1,11 @@
-import React,{useeffect,useState} from "react";
+import React, { useeffect, useState } from "react";
 
 const DomInfoDialogBox = ({ id,idx,clsname,parentId,parentClass,children,top,left,onClose}) => {
-  const [displayArray , setdisplayArray] = useState("2");  
+  const [displayArray, setdisplayArray] = useState("2");
   const handleSeemore = () => {
     setdisplayArray(children.length);
- }; 
- const leftover = 
-  children.length - displayArray;
-;
+  };
+  const leftover = children.length - displayArray;
   return (
     <div>
       <div
@@ -17,44 +15,47 @@ const DomInfoDialogBox = ({ id,idx,clsname,parentId,parentClass,children,top,lef
           left: `${left}px`,
         }}
       >
-        <div style="float:right">
-          <button id="closedompeeker" onClick={() => onClose(idx)}>
+        <div className="dom-close-btn">
+          <button
+            id="closedompeeker"
+            className="close-btn-style"
+            onClick={() => onClose(idx)}
+          >
             X
           </button>
         </div>
         <div>
-          <h1>Element ID: {id}</h1>
-          <h2>Class: {clsname}</h2>
-          <h2>Parent ID: {parentId}</h2>
-          <h2>Parent Class: {parentClass}</h2>
+          <div className="dom-basic">Element ID: {id}</div>
+          <div className="dom-basic">Class: {clsname}</div>
+          <div className="dom-dialog">Parent ID:</div>
+          <div className="dom-details">{parentId}</div>
+          <div className="dom-dialog">Children[{children.length}]</div>
+          <div className="dom-details" align="center">
+            <table>
+              <tr>
+                <td className="details-table">
+                  <p> Child ID</p>
+                  <ul>
+                    {children.slice(0, displayArray).map((val) => (
+                      <li>{val.id}</li>
+                    ))}
+                  </ul>
+                </td>
 
-          <h2># of Children Element: {children.length}</h2>
-
-          <p>Child elements </p>
-          <table align = "center">
-            <tr>
-              <td>
-                <p> Child ID</p>
-                <ul>
-                  {children.slice(0,displayArray).map((val) => (
-                    <li>{val.id}</li>
-                  ))}
-                </ul>
-              </td>
-
-              <td>
-                <p>Child classes:</p>
-                <ul>
-                  {children.slice(0,displayArray).map((val) => (
-                    <li>{val.class}</li>
-                  ))}
-                </ul>
-              </td>
-            </tr>
-          </table>
-          <button id="closedompeeker" style={{cursor: "pointer"}} onClick={handleSeemore} >
-                see  {leftover} more
-          </button>
+                <td className="details-table">
+                  <p>Child classes:</p>
+                  <ul>
+                    {children.slice(0, displayArray).map((val) => (
+                      <li>{val.class}</li>
+                    ))}
+                  </ul>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div id="closedompeeker" className="see-more" onClick={handleSeemore}>
+            ... {leftover} more
+          </div>
         </div>
       </div>
     </div>
