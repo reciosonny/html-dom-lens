5a3ffbacf696cbc16f9552c817686abbdfb6be64
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2021-10-18 16:29:21 +0800

    Task-18-for-Testing

--- react-client/src/Components/App.js
+++ react-client/src/Components/App.js
@@ -20,6 +20,11 @@ function App() {
     setdomSwitch(!domSwitch);
   };
   const refDomHighlight = React.useRef(null);
+  const uuidv4 = require("uuid/v4")
+  const originalID = "";
+  const colorselection = ["#311B92", "#4527A0", "#512DA8","#5E35B1","#673AB7","#7E57C2","#9575CD","#B39DDB","#D1C4E9","#EDE7F6","#E91E63","#D81B60","#C2185B","#AD1457","#880E4F","#EC407A","#F06292","#F48FB1","#F8BBD0","#FCE4EC","#263238","#37474F","#455A64","#546E7A","#607D8B","#78909C","#90A4AE","#B0BEC5","#CFD8DC","#ECEFF1"];
+
+ 
 
   useEffect(() => {
     if (!PRODUCTION_MODE) {
@@ -59,6 +64,13 @@ function App() {
         });
 
         var eltarget = e.target;
+
+        var randomCode = "uniqueID"+uuidv4();
+        if(e.target.id === null || e.target.id.trim() === "")
+        {
+          e.target.setAttribute("id" , randomCode);                  
+        }
+
         const elComputedStyle = ["font-size", "color", "font-family"].reduce(
           (init, curr) => ({
             ...init,
@@ -89,13 +101,17 @@ function App() {
 
         const pageYcoordinate = e.pageY;
 
+        const randomcolor = Math.floor(Math.random() * colorselection.length);      
+        eltarget.style.cssText += 'border:3px solid;border-color:'+ `${colorselection[randomcolor]}`;     
+        
         await setDomInfo(value => {
 
           return [...value,
             {
               x: e.pageX,
               y: pageYcoordinate + 100,
-              id: e.target.id.trim() !== "" ? "#" + e.target.id.trim() : null,
+              // id: eltarget.id.trim() !== "" ? "#" + eltarget.id.trim() : null,
+              id: originalID.trim() !== "" ? "#" + originalID.trim() : null,
               clstag: e.target.localName,
               clsname: clsArr,
               children: children,
@@ -103,6 +119,8 @@ function App() {
               size: elComputedStyle["font-size"],
               textcolor: colorhex,
               family: elComputedStyle["font-family"].replaceAll('"', ''),
+              bordercolor: colorselection[randomcolor],
+              uniqueID:  eltarget.id.trim()
             },
           ]
         });
@@ -171,9 +189,17 @@ function App() {
     injectDOMEventInBody();
   };
 
-  const handleRemoveDialogBox = (idx) => {
+  const handleRemoveDialogBox = (idx,id,uniqueID) => {
     const newDomInfo = domInfo.filter((x, currentIdx) => currentIdx !== idx);
-
+    const removeDomborder = domInfo.filter((x, currentIdx) => currentIdx === uniqueID);
+    if (id !== null)
+    {      
+      document.querySelector('#'+id).style.removeProperty('border');
+    }
+    else
+    {
+      document.querySelector('#'+uniqueID).style.removeProperty('border');
+    }
     setDomInfo(newDomInfo);
   };
 
@@ -204,6 +230,8 @@ function App() {
               fontsize={domInfo.size}
               fontfamily={domInfo.family}
               textcolor={domInfo.textcolor}
+              borderclr = {domInfo.bordercolor}
+              uniqueID ={domInfo.uniqueID}
             />
           ))}
 
