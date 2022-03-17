5eacc9bf0dc02792a5e626ad7fe403acccacf03f
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2022-03-17 17:02:09 +0800

    Task-54-Edit-function-bookmark

--- react-client/src/Components/BookmarkPanel/BookmarkInfo.js
+++ react-client/src/Components/BookmarkPanel/BookmarkInfo.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { RiPencilFill } from "react-icons/ri";
 import { FaTrash } from "react-icons/fa";
 
-const BookmarkInfo = ({ bookmarkHidden, onCloseBookmark, onEdit, onRemove, bookmarks, onClickBookmarkList }) => (
-  
+const BookmarkInfo = ({ bookmarkHidden, onCloseBookmark, onEdit, onRemove, bookmarks, onClickBookmarkList }) => (  
   <React.Fragment>
     <div className='card-bookmark' hidden={bookmarkHidden}>
       <span className='bookmark-header'>
@@ -28,20 +27,34 @@ const BookmarkInfo = ({ bookmarkHidden, onCloseBookmark, onEdit, onRemove, bookm
   </React.Fragment>
 );
 
-const BookmarkItem = ({ data, index, onRemove, onClickBookmarkList }) => {
+const BookmarkItem = ({ data, index, onRemove, onClickBookmarkList, onEdit }) => {
+  const [displayInput, setDisplayInput] = useState(true);
+  const [txtInput, setTxtInput] = useState("");
 
-  const onEdit = () => {
-    alert('editing a bookmark details');
+  const onSubmitBookmark = (e) => {    
+    onEdit(e, txtInput, data.id);
+    setDisplayInput(true)
   }
 
-  // const onRemove = () => {
-    
-  // }
+  const onClickEdit = () => {
+    setDisplayInput(false)
+  } 
 
   return (
     <li className='bookmark__list-item' key={index} data-bookmark-id={data.id} onClick={onClickBookmarkList}>
-      <div className='list__item-details'>        
-        <label className='lbl-title'>{data.title}</label>
+      <div className='list__item-details' >
+      {displayInput ? 
+          <label className="lbl-title">{data.title}</label> : 
+          <form className="frm-panel"  onSubmit={onSubmitBookmark}>
+            <input
+              onChange={(e) => setTxtInput(e.target.value)}
+              type="text"
+              value={txtInput}          
+              className="txt-title"
+              autoFocus
+            />
+          </form>
+        }
         <label className='lbl-elem'>{data.elem}</label>
         <label className='lbl-classes' style={{ fontSize: "12px" }}>
           {data.classes}
@@ -49,7 +62,7 @@ const BookmarkItem = ({ data, index, onRemove, onClickBookmarkList }) => {
       </div>
       <span className='list__item-options'>
         <button>
-          <RiPencilFill data-bookmark-id={data.id} onClick={onEdit} color="#607D8B"/>
+          <RiPencilFill data-bookmark-id={data.id} onClick={onClickEdit} color="#607D8B"/>
         </button>
         <button data-bookmark-id={data.id} onClick={onRemove}>
           <FaTrash  color="#607D8B"/>
